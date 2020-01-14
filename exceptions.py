ef875ebf39e10e25e8b5e3ff8183be5baae9011c
Mike Harley <mharley12345@gmail.com>
2020-01-13 18:15:52 -0800

    .

---
+++ exceptions.py
@@ -0,0 +1,7 @@
+# This will throw an error
+try:
+    int("a")
+except ValueError:
+    print("oops, you can't do that")
+
+print("this is the end of my program")
---
+++ name_lib.py
@@ -0,0 +1,10 @@
+def upper_case_name(name):
+    return name.upper()
+if __name__ == "__main__":
+
+    name = "Mike"
+    name_upper = upper_case_name(name)
+    print(f"Upper case name is {name_upper}")
+    
+    
+    print("dunder name",__name__)
